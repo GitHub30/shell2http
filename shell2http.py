55b616a79d3bb8532f90fcf968e68ebcf9a21469
Mondhs <mondhs@users.noreply.github.com>
2022-11-25 15:03:37 +0000

    added argumet to send back output

--- shell2http.py
+++ shell2http.py
@@ -1,20 +1,29 @@
-from subprocess import Popen
+import subprocess
 from argparse import ArgumentParser
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 routes = {}
 
+def shellHTTPRequestHandler(is_output)
+    print("is_output",is_output)
+    class ShellHTTPRequestHandler(BaseHTTPRequestHandler):
+        def do_GET(self):
+            self.send_response(200)
+            self.end_headers()
+            if self.path in routes:
+                if(is_output):
+                    output = subprocess.Popen(routes[self.path], stdout=subprocess.PIPE, shell=True).communicate()[0].strip()
+                    self.wfile.write(output)
+                else:
+                    subprocess.Popen(routes[self.path], shell=True)
 
-class ShellHTTPRequestHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        if self.path in routes:
-            Popen(routes[self.path], shell=True)
+                
+    return ShellHTTPRequestHandler
 
 
 def serve():
     parser = ArgumentParser()
+    parser.add_argument("-o","--output",action="store_true",help="Send back ouput")
     parser.add_argument('-p', '--port', default=8080, type=int)
     parser.add_argument('command', nargs='+')
     args = parser.parse_args()
@@ -26,7 +35,7 @@ def serve():
     routes = dict(zip(args.command[0::2], args.command[1::2]))
     for path, command in routes.items():
         print(f'http://localhost:{args.port}{path}', command)
-    with HTTPServer(('', args.port), ShellHTTPRequestHandler) as httpd:
+    with HTTPServer(('', args.port), shellHTTPRequestHandler(args.ouput)) as httpd:
         httpd.serve_forever()
 
 
