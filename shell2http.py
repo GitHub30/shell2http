546f40679df6baddc26ac39067db3e551cfec707
言葉 <funaox@gmail.com>
2022-11-27 13:21:26 +0900

    Update shell2http.py

--- shell2http.py
+++ shell2http.py
@@ -1,8 +1,10 @@
 import os
 import subprocess
-from argparse import ArgumentParser
 from urllib.parse import urlparse, parse_qs
+from argparse import ArgumentParser
 from http.server import BaseHTTPRequestHandler, HTTPServer
+
+index_page = '<!DOCTYPE html><html><head><title>Index</title><style>a{display:block;margin:16px}</style></head><body>%s</body></html>'
 
 
 def shellHTTPRequestHandler(args):
@@ -21,8 +23,13 @@ def shellHTTPRequestHandler(args):
             if args.sse:
                 self.send_header('Content-Type', 'text/event-stream')
             self.end_headers()
-            if args.add_exit and parsed_url.path == '/exit':
-                exit()
+            if parsed_url.path == '/' and '/' not in routes and not args.no_index:
+                paths = list(routes.keys())
+                if args.add_exit:
+                    paths.append('/exit')
+                html = (index_page % ''.join(
+                    map(lambda p: f'<a href={p} target=_blank>{p}</a>', paths)))
+                self.wfile.write(html.encode('utf-8'))
             if parsed_url.path in routes:
                 kwargs = {
                     'args': routes[parsed_url.path],
@@ -38,6 +45,8 @@ def shellHTTPRequestHandler(args):
                 if args.output:
                     for line in proc.stdout:
                         self.wfile.write(line)
+            if parsed_url.path == '/exit' and args.add_exit:
+                exit()
 
     return ShellHTTPRequestHandler
 
@@ -46,6 +55,8 @@ def serve():
     parser = ArgumentParser()
     parser.add_argument('-form', action='store_true',
                         help='parse query into environment vars')
+    parser.add_argument('-no-index', action='store_true',
+                        help='do not generate index page')
     parser.add_argument('-add-exit', action='store_true',
                         help='add /exit command')
     parser.add_argument('-output', action='store_true',
